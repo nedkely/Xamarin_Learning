1b33ac512c012cdfa80900260a3e50c58f5b2bf5
nedkely <nedkely@gmail.com>
2022-01-19 07:48:58 +0330

    Add project files.

---
+++ Xamarin_Learning/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin_Learning.ViewModels;
+
+namespace Xamarin_Learning.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Xamarin_Learning/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Xamarin_Learning.Models;
+using Xamarin_Learning.ViewModels;
+
+namespace Xamarin_Learning.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
